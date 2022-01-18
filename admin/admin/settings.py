cb50f99abda145b366df62c7f9e9c3b8cacd7115
leandro2585 <leo.real2585@gmail.com>
2022-01-17 21:24:18 -0300

    feat: add migrations

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -8,16 +8,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'games'
-]
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'corsheaders',
+        'games'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- admin/games/models.py
+++ admin/games/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length = 200)
+    image = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 200)
+    quantity_in_cart = models.PositiveIntegerField(default = 0)
+
+class User(models.Model):
+    pass
