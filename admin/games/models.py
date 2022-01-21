1b9d0158eb5e20a0a2ec7e19d7c8bf42bfa18caa
leandro2585 <leo.real2585@gmail.com>
2022-01-21 18:34:17 -0300

    feat: add price to game model

--- admin/games/models.py
+++ admin/games/models.py
@@ -5,6 +5,7 @@ class Game(models.Model):
     image = models.CharField(max_length = 200)
     description = models.CharField(max_length = 200)
     quantity_in_cart = models.PositiveIntegerField(default = 0)
+    price = models.PositiveIntegerField(default = 200)
 
 class User(models.Model):
     pass
